a6760538d95874ff01cf8cad789591ea00e28005
Versus7 <53363059+Versus7@users.noreply.github.com>
2021-01-30 19:38:48 -0800

    Add files via upload

--- typing.py
+++ typing.py
@@ -2,11 +2,27 @@
 import time
 import random
 import pyautogui
+import pytesseract
 from PIL import ImageGrab
 from PIL import Image
 
 #functions
 
-def typing(y,y2):
-    filepath = "typeMonke.png"
-    
+def typing(x,x2,y,y2):
+    if x >= x2 or y >= y2:
+        print("Check your values.")
+    else:
+        time.sleep(3)
+        filepath = "typing.png"
+        screenshot = ImageGrab.grab(bbox=(x,y,x2,y2))
+        screenshot.save(filepath)
+        screenshot.show()
+        text = pytesseract.image_to_string(screenshot)
+        print(text)
+
+#preset 1: monkeytype
+x, x2, y, y2 = 500, 2600, 1450, 1750
+
+
+#execution
+typing(x, x2, y, y2)
